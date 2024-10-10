d4afd228657b5262cef3596e5dc52f65638bfb70
Cristobal Aleman <Cristobal.Aleman@tecalliance.net>
2024-10-10 10:17:02 +0100

    TecDoc http client TecDoc preparation

---
+++ ProductManufactureService/HttpClients/DTOs/GetArticlesDTO.cs
@@ -0,0 +1,18 @@
+﻿namespace ProductManufacturerService.HttpClients.DTOs
+{
+    public class GetArticlesDTO
+    {
+        public string SearchQuery { get; set; } = "100"; // This is the search term you need to collect in your endpoint and send to getArticles
+        public string ArticleCountry { get; set; } = "GB";
+        public string Lang { get; set; } = "en";
+        public string SearchMatchType { get; set; } = "prefix_or_suffix";
+        public int SearchType { get; set; } = 0;
+        public int Page { get; set; } = 1;
+        public int PerPage { get; set; } = 100;
+        public bool IncludeAll { get; set; } = false;
+        public bool IncludeLinkages { get; set; } = true;
+        public int linkagesPerPage { get; set; } = 100;
+        public int Provider { get; set; } = 2080;
+
+    }
+}
---
+++ ProductManufactureService/HttpClients/TecDoc/TecDocApiClient.cs
@@ -0,0 +1,34 @@
+﻿namespace ProductManufacturerService.HttpClients.TecDoc
+{
+    public class TecDocApiClient
+    {
+        private readonly HttpClient _httpClient;
+        private readonly IConfiguration _configuration;
+        private readonly ILogger _logger;
+        private readonly string _url;
+        public TecDocApiClient(HttpClient httpClient, IConfiguration configuration, ILogger logger)
+        {
+            _httpClient = httpClient;
+            _configuration = configuration;
+            _logger = logger;
+            _url = _configuration["TecDocAPI"];
+        }
+        public async Task GetArticles()
+        {
+            var response = await _httpClient.PostAsync(_url, Content);
+            response.EnsureSuccessStatusCode();
+
+            var content = await response.Content.ReadAsStringAsync();
+            return content;
+        }
+
+        public async Task GetAmBrandAddress()
+        {
+            var response = await _httpClient.PostAsync(_url, Content);
+            response.EnsureSuccessStatusCode();
+
+            var content = await response.Content.ReadAsStringAsync();
+            return content;
+        }
+    }
+}
